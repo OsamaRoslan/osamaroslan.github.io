22f5d164181744833c127cbd972384267c6423c3
Osama Roslan <17204679@siswa.um.edu.my>
2023-07-27 00:59:25 +0800

    update html

--- fyp/tip_um/members/views.py
+++ fyp/tip_um/members/views.py
@@ -1,7 +1,30 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-  template = loader.get_template('myfirst.html')
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def index(request):
+  template = loader.get_template('index.html')
   return HttpResponse(template.render())
 
+def testing(request):
+  template = loader.get_template('testing.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
