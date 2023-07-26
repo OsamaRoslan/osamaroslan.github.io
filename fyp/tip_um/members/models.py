3b5a02e001f7ac7d5de0c919d1ad2a6cda08f08f
Osama Roslan <17204679@siswa.um.edu.my>
2023-07-27 01:00:54 +0800

    move project to another project

--- fyp/tip_um/members/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Member(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  email = models.TextField(null=True)
-  joined_date = models.DateField(null=True)
-
--- fyp/tip_um/members/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('members/', views.members, name='members'),
-    path('members/details/<int:id>', views.details, name='details'),
-    path('testing/', views.testing, name='testing'), 
-]
